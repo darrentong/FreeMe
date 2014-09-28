3daa495ca6f7db4ca903c1e7494497713536128c
Evan Qi <evanqi1230@gmail.com>
2014-09-28 04:19:32 -0400

    parses class assignment information and sends to /calendar

--- routes/index.js
+++ routes/index.js
@@ -9,21 +9,67 @@ router.get('/', function(req, res) {
 
 /* POST to Calendar Rendering */
 router.post('/calendar', function(req, res) {
-	res.render('calendar', { });
+	
 	var db = req.db;
 
 	// Get body of class file
 	var str = req.body.classes;
+	console.log(str);
 	var arr = str.trim().split(",");
 	var hash = {};
 	
 	var collection = db.get('syllabus');
 
+	var hash = {};
 	for (i=0; i < arr.length; i++) {
-	    collection.find({catalogId: arr[i]},{},function(e,docs){
-		    console.log(docs);
-		});
+	    collection.find({catalogId: arr[i]},{},function(e, docs){
+	    	var docsKeys = Object.keys(docs);
+	    	for(j=0; j<docsKeys.length; j++) {
+	    		if(docs[j].hasOwnProperty('assignment')) {
+	    			currentAssignments = docs[j].assignment;
+	    			for(k=0; k<currentAssignments.length; k++) {
+	    				if(hash[currentAssignments[k].dueDate] === undefined) {
+	    					hash[currentAssignments[k].dueDate] = {};
+	    					hash[currentAssignments[k].dueDate][currentAssignments[k].name] = 1;
+	    				}
+	    				else {
+	    					hash[currentAssignments[k].dueDate][currentAssignments[k].name] = 1;
+	    				}
+	    			}
+	    		}
 
+	    		if(docs[j].hasOwnProperty('quiz')) {
+	    			currentQuizzes = docs[j].quiz;
+	    			for(k=0; k<currentQuizzes.length; k++) {
+	    				if(hash[currentQuizzes[k].dueDate] === undefined) {
+	    					hash[currentQuizzes[k].dueDate] = {};
+	    					hash[currentQuizzes[k].dueDate][currentQuizzes[k].name] = 2;
+	    				}
+	    				else {
+	    					hash[currentQuizzes[k].dueDate][currentQuizzes[k].name] = 2;
+	    				}
+	    			}
+	    		}
+
+	    		if(docs[j].hasOwnProperty('exam')) {
+	    			currentExams = docs[j].exam;
+	    			for(k=0; k<currentExams.length; k++) {
+	    				if(hash[currentExams[k].dueDate] === undefined) {
+	    					hash[currentExams[k].dueDate] = {};
+	    					hash[currentExams[k].dueDate][currentExams[k].name] = 5;
+	    				}
+	    				else {
+	    					hash[currentExams[k].dueDate][currentExams[k].name] = 5;
+	    				}
+	    			}
+	    		}
+
+	    	}
+
+		});
+		res.render('calendar', { 
+	    	"workload" : hash
+	    });
 	}
 	
 	
